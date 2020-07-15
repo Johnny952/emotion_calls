22a7b22c51252589d61dbef5fc40139feac53ce0
Javier <javier.smith.dlc@gmail.com>
2020-07-15 01:32:06 -0400

    Añade documentación a detect_emotion.py

--- detect_emotion.py
+++ detect_emotion.py
@@ -8,14 +8,32 @@ import pandas as pd
 from joblib import dump, load
 import matplotlib.pyplot as plt
 
+# Parameters for audio feature extraction
 mw = "1.0"
 ms = "1.0"
 sw = "0.050"
 ss = "0.050"
+# Labels of emotions
 EMOTIONS = ["neutral", "calm","happy", "sad", "angry", "fearful", "disgust", "surprised"]
 
 def split_audio(audiofile, step_size):
+	"""
+	splits audio, located in path audiofile, in several segments of step_size [ms]
+	length each
+	--------------
+	Arguments
+	-------------
+	audiofile: string
+		path to audio file
+	step_size: int
+		length, in milliseconds, of each audio segment
+	-------------
+	Returns: string
+		path to folder with audio segments
+	"""
+
 	output_dir = "/".join(audiofile.split("/")[:-1]) + "/splits"
+	# Creates output directory for audio splits
 	if(os.path.isdir(output_dir)): 
 		shutil.rmtree(output_dir)
 		os.mkdir(output_dir)
@@ -24,30 +42,48 @@ def split_audio(audiofile, step_size):
 
 	filename = audiofile.split("/")[-1].split(".")[0] 
 	audio = AudioSegment.from_wav(audiofile) 
-	t = 0
-	window_counter = 1
-	has_length = True
+	t = 0 
+	window_counter = 1 # split id
+	has_length = True 
+	# Continues until entire length of audio file is segmented
 	while(has_length):
-		audio_window = audio[t:t+step_size]
+		audio_window = audio[t:t+step_size] # audio split
+		# checks whether audio split is of appropiate size
 		if(len(audio_window)==step_size):
 			audio_window.export(output_dir + "/"+ filename+"_"+str(window_counter)+".wav",format='wav')
 			window_counter += int(step_size/1000)
 			t += step_size
+		# if audio split is smaller than step_size, breaks the loop
 		else:
 			has_length=False
 	return output_dir
 
 def preprocess_audio(audio_dir):
+	"""
+	extracts short-term and mid-term audio features from each split audio file in audio_dir 
+	and returns as a pandas dataframe with time indications
+	-------------
+	Arguments
+	-------------
+	audio_dir: string
+		path to folder with audio splits
+	-------------
+	Returns: DataFrame
+		table with extracted audiofeatures according to their relative time in general audio file
+	"""
+	# Extracts short and mid term features of each audio file and stores
+	# them in the same folder
 	os.system('python3 ./pyAudioAnalysis/pyAudioAnalysis/audioAnalysis.py featureExtractionDir -i {} -mw {} -ms {} -sw {} -ss {}'.format(audio_dir, mw, ms, sw, ss))
 	filenames = glob.glob(audio_dir+"/*.npy")
 	df = []
-	clf = load("./models/emotion_clf.joblib")
+	# Loads each feature matrix, flattens it and stores it alongside corresponding time
 	for filepath_idx in tqdm(range(len(filenames))):
 		filepath = filenames[filepath_idx]
 		second = filepath.split("/")[-1].split(".")[0].split("_")[-1]
 		data = np.load(filepath).flatten()
 		df.append([second,data])
 	df = pd.DataFrame(df,columns=['second','feats']).dropna()
+	# Drops rows with null or infinite values
 	invalid_rows = []
 	for index,row in df.iterrows():
 		feats = np.array(row['feats'])
@@ -57,11 +93,28 @@ def preprocess_audio(audio_dir):
 	return df
 
 def predict_audio(preprocessed):
+	"""
+	predicts emotions over each sample of preprocessed audio files
+	-------------
+	Arguments
+	-------------
+	preprocessed: DataFrame
+		table were each row is a feature vector from an audio file and its
+		correspoding time
+	-------------
+	Returns: DataFrame
+		table were each row contains a vector with emotion predictions in percentages
+		and the time relative to the general audio file
+	"""
 	
 	global EMOTIONS
+	# Loads trained classifications model
 	clf = load("./models/emotion_clf.joblib")
 	df = preprocessed
+	# Predicts a probability for each emotion in every audio sample
 	predictions = clf.predict_proba(df['feats'].to_list())
+	# Arranges predictions as percentages and includes time relative to general
+	# audio file
 	display_table = pd.DataFrame(predictions, columns = EMOTIONS)*100
 	display_table = display_table.round(decimals=1)
 	display_table["second"] = list(map(int, df["second"]))
@@ -69,20 +122,37 @@ def predict_audio(preprocessed):
 	return display_table
 
 
-def main(path):
+def main(path, display=False):
+	"""
+	main function for handling audio emotion prediction
+	-------------
+	Arguments
+	-------------
+	path: string
+		path to audio file
+	-------------
+	Returns: DataFrame
+		table with emotion predictions for each time step
+	"""
+	# Splits audio file into several audio segments of mw [s] of length
 	audio_dir = split_audio(path, float(mw)*1000)
+	# Extracts features for each audio segment
 	preprocessed = preprocess_audio(audio_dir)
+	# Predicts emotions for each audio segment
 	table = predict_audio(preprocessed)
 
-	table.iloc[::5,:].plot(x="second", y=EMOTIONS, kind="line",linewidth=2)
-	plt.title("{} emotions".format(path.split("/")[-1].split(".")[0]))
-	plt.xlabel("Tiempo [segundos]")
-	plt.ylabel("Porcentaje")
-	plt.show()
+	if(display=True):
+		# Displays emotions relative to time from audio in path
+		table.iloc[::5,:].plot(x="second", y=EMOTIONS, kind="line",linewidth=2)
+		plt.title("{} emotions".format(path.split("/")[-1].split(".")[0]))
+		plt.xlabel("Tiempo [segundos]")
+		plt.ylabel("Porcentaje")
+		plt.show()
 	return table
 
 
 if __name__ == '__main__':
+	# Audio test samples
 	thunderstrack = "./test_audios/thunderstrack.wav"
 	seu_jorge = "./test_audios/seu_jorge.wav"
-	results_thunderstrack = main(thunderstrack)
+	results_thunderstrack = main(thunderstrack, display=True)
